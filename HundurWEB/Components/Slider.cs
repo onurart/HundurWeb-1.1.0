cd08578de2e3987a11b03ab9fec8baaabc964c6e
onur  umutluoglu <onurumutluoglu@gmail.com>
2021-12-15 17:31:42 +0300

    Test

---
+++ HundurWEB/Components/Slider.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HundurWEB.Components
+{
+    public class Slider : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View();
+        }
+    }
+}
